2822d276b99c4fc3d4d908bd5be58833f8facc0a
Lawal Daniel Aramide <Kanayo@Aramide.local>
2026-04-22 17:38:17 +0100

    Add CI/CD pipeline

--- api/main.py
+++ api/main.py
@@ -5,7 +5,14 @@ import os
 
 app = FastAPI()
 
-r = redis.Redis(host="localhost", port=6379)
+REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
+
+r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT,decode_responses=True)
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
 
 @app.post("/jobs")
 def create_job():
@@ -17,6 +24,6 @@ def create_job():
 @app.get("/jobs/{job_id}")
 def get_job(job_id: str):
     status = r.hget(f"job:{job_id}", "status")
-    if not status:
-        return {"error": "not found"}
-    return {"job_id": job_id, "status": status.decode()}
+    if status is None:
+        return {"error": "job not found"}
+    return {"job_id": job_id, "status": status}
--- worker/worker.py
+++ worker/worker.py
@@ -2,17 +2,41 @@ import redis
 import time
 import os
 import signal
+import sys
 
-r = redis.Redis(host="localhost", port=6379)
+REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
+
+r = redis.Redis(
+    host=REDIS_HOST,
+    port=REDIS_PORT,
+    decode_responses=True  # important fix
+)
+
+running = True
+
+
+def shutdown(sig, frame):
+    global running
+    print("Shutting down worker...")
+    running = False
+    sys.exit(0)
+
+
+signal.signal(signal.SIGINT, shutdown)
+signal.signal(signal.SIGTERM, shutdown)
+
 
 def process_job(job_id):
     print(f"Processing job {job_id}")
-    time.sleep(2)  # simulate work
+    time.sleep(2)
     r.hset(f"job:{job_id}", "status", "completed")
     print(f"Done: {job_id}")
 
-while True:
+
+while running:
     job = r.brpop("job", timeout=5)
+
     if job:
         _, job_id = job
-        process_job(job_id.decode())
+        process_job(job_id)
